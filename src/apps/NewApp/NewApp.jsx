af4b757378ae2f01ce30fa4793873af51ff89787
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-13 11:31:44 +0200

    Update NewApp.jsx

--- src/apps/NewApp/NewApp.jsx
+++ src/apps/NewApp/NewApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-import Profile from './components/Profile';
+import File from './components/Profile';
 import ProtectedRoute from './components/ProtectedRoute';
 
 const NewApp = () => {
